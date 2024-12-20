8c5c67ebb360a080631498d3bf90d07a1b1cf0ea
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-20 13:55:11 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Marko Zivanovic on 20.12.24..
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
