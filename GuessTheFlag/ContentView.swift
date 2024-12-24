a15f9b35c4e45289b987b4364cfe08c064f45231
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-24 21:42:10 +0100

    ShowingScoreWithAnAlert

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
-//            Color.blue
-//                .ignoresSafeArea()
+            //            Color.blue
+            //                .ignoresSafeArea()
             LinearGradient(colors: [.black, .blue], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 30) {
@@ -32,7 +34,7 @@ struct ContentView: View {
                 ForEach(0..<3) { number in
                     Button {
                         //MARK: Flag was tapped
-                        
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -40,7 +42,27 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("You score is \(scoreTitle)")
+        }
     }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "You got it!"
+        } else {
+            scoreTitle = "Wrong!"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
 }
 
 #Preview {
